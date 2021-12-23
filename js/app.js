a634a63c53ff3f28cada272b64b47f52ae9f1ff3
Diego Fernández <68358400+dfernandezfb@users.noreply.github.com>
2021-12-23 18:24:50 -0300

    Revert "template"

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 
 
+
+notas.forEach(nota=>{
+  let tablero = document.querySelector('.tablero');
+  let notita = document.createElement('div');
+  notita.innerHTML=`<p>${nota}</p>`;
+  notita.classList.add('note-example', 'm-2');
+  tablero.appendChild(notita);
+})
+
+
+
+
+
