250c87fa4faa4205a472cc1f5a8e9f3c46113b79
Diego Fernández <dfernandezfb@gmail.com>
2021-12-23 20:43:45 -0300

    adding notes done

--- js/app.js
+++ js/app.js
@@ -29,8 +29,7 @@ notes.forEach(noteText =>{
 
 const noteContent = document.querySelector('#note-text');
 const exampleNote = document.querySelector('#note-example');
-console.log(noteContent);
-console.log(exampleNote);
+
 noteContent.addEventListener('keyup',()=>{
   if(noteContent.value===''){
     exampleNote.innerText='Escriba una nota';
@@ -38,3 +37,29 @@ noteContent.addEventListener('keyup',()=>{
     exampleNote.innerText=noteContent.value;
   }
 })
+
+
+//* Guardar los datos de nueva nota en LS y mostrarlos (Create/Update) //DIEGO
+
+const addNote = (event) => {
+  event.preventDefault();
+
+  // TODO Guardar en LS
+  //! Traer lo que haya guardado en LS
+  const notesLS = JSON.parse(localStorage.getItem('notes'));
+  //! Editamos los datos que trajimos con la nueva nota
+  notesLS.push(noteContent.value);
+  //! Volver a mandarlo a LS
+  localStorage.setItem('notes',JSON.stringify(notesLS));
+
+  //TODO Mostrar la nueva nota en el DOM
+  const note = document.createElement('div');
+  note.innerText = noteContent.value;
+  note.classList.add('note-style','m-3')
+  const noteContainer = document.querySelector('.tablero');
+  noteContainer.appendChild(note);
+
+  //TODO Borrar infor de la nota de ejemplo y del form
+  document.querySelector('form').reset();
+  exampleNote.innerText = 'Escriba una nota';
+}
