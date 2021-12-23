9971c1ce5368bb111dd70030a6effb5cda8f866c
Diego Fernández <dfernandezfb@gmail.com>
2021-12-23 18:22:15 -0300

    template

--- js/app.js
+++ js/app.js
@@ -1,15 +1,2 @@
 
 
-
-notas.forEach(nota=>{
-  let tablero = document.querySelector('.tablero');
-  let notita = document.createElement('div');
-  notita.innerHTML=`<p>${nota}</p>`;
-  notita.classList.add('note-example', 'm-2');
-  tablero.appendChild(notita);
-})
-
-
-
-
-
